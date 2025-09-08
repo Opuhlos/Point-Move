b5f3a5adb7bead69a9077eed449321a62dc47a65
Eric Nguyen <ericnguyen455@gmail.com>
2025-09-07 21:26:04 -0400

    point and click navigation pathfinding

--- point-move/scripts/player.gd
+++ point-move/scripts/player.gd
@@ -1,15 +1,25 @@
 extends CharacterBody2D
 
-var destination: Vector2
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+
 var speed: float = 200
 
+func _ready() -> void:
+	navigation_agent.radius	= 10.0
+
 func _process(delta: float) -> void:
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		destination = get_global_mouse_position()
+	pass
 
 func _physics_process(delta: float) -> void:
-	if (position.distance_to(destination) > 3):
-		velocity = position.direction_to(destination).normalized() * speed
-		move_and_slide()
-	else:
+	if Input.is_action_just_pressed("mouse_left"):
+		navigation_agent.target_position = get_global_mouse_position()
+	
+	if navigation_agent.is_navigation_finished():
 		Singleton.player_stationed.emit()
+		return
+		
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+	velocity = global_position.direction_to(next_path_position) * speed
+	print(next_path_position)
+
+	move_and_slide()
